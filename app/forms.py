f7a4064b06539bb7065005af36bafc2da4f3d87a
Smalls141414 <ari.grady14@gmail.com>
2022-12-02 14:31:53 -0800

    Added SignUp page and removed .DS_Store

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,9 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
 class SignUpForm(FlaskForm):
-    newUsername = StringField('Username', validators=[DataRequired()])
-    newPassword = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
